571bbc20d68867e6cdc05e356ed20b90bba78d25
Noah Rollins <noahjrollins@gmail.com>
2022-11-01 09:07:35 -0500

    commit tree 5d0f097cbc62c1df50a0b696ac00e2c627f7cd83

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity) {
+        if (activity === undefined){ 
+    return `This Saturday, I want to roller-skate!`
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }   
+}
+
+function mondayWork (todo) {
+    if (todo === undefined) {
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${todo}.`
+    }
+}  
+
+function wrapAdjective( x = "*"){
+  return function(y = "special"){
+    return `You are ${x}${y}${x}!`
+  }
+    
+}
